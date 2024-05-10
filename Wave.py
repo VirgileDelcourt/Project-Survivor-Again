1368b03f69f2687ab4a52d768f88dca1553cd607
VirgileDelcourt <vdelcourtdegeetere@gmail.com>
2024-05-10 17:55:07 +0200

    Ajout de # dans Wave.py

--- Wave.py
+++ Wave.py
@@ -3,9 +3,10 @@ from random import choices
 
 class Wave:
     def __init__(self, time, *args):
-        self.time = time
-        self.enemies = []
-        self.probas = []
+        self.time = time  # temps à partir de quoi la vague doit arriver
+        self.enemies = []  # liste de tous les ennemis qui peuvent spawner durant cette vague
+        self.probas = []  # liste des probabilités pour chaque ennemi de spawner
+        # les indices de enemies et probas correspondent (probas[0] est la probabilité de enemies[0] de spawn)
         for tup in args:
             self.enemies.append(tup[0])
             self.probas.append(tup[1])
